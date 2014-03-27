c2d1191bba87ea302d0226ae59c1216a9a91def7
morrj140 <james.m.morris@disney.com>
2014-03-27 10:58:11 -0400

    Signed-off-by: morrj140 <james.m.morris@disney.com>

---
+++ nl_pattern_graph_concepts.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+#
+# Natural Language Processing of PMO Information
+#
+import os
+from nl_lib import Logger
+from nl_lib.Concepts import Concepts
+from nl_lib.ConceptGraph import PatternGraph, NetworkXGraph
+from nl_lib.Constants import *
+
+logger = Logger.setupLogging(__name__)
+   
+if __name__ == "__main__":
+    listConceptFile = list()
+    #listConceptFile.append(projectsFile)
+    listConceptFile.append(peopleFile)
+    #listConceptFile.append(wordsFile)
+    #listConceptFile.append(topicsFile)
+    #listConceptFile.append(sentencesFile)
+    #listConceptFile.append(similarityFile)
+
+    #graph = NetworkXGraph()
+    graph = PatternGraph()
+
+    for conceptFile in listConceptFile:
+        logger.info("Loading :" + conceptFile)
+        concepts = Concepts.loadConcepts(conceptFile)
+
+        graph.addConcepts(concepts)
+
+    if isinstance(graph, PatternGraph):
+        #graph.g.remove("ProjectConceptsSimilarity")
+        logger.info("Exporting Graph")
+        graph.exportGraph()
+
+    if isinstance(graph, NetworkXGraph):
+        #graph.G.remove_node("ProjectConceptsSimilarity")
+        graph.drawGraph("concepts.png")
+        filename = "concepts.net"
+        logger.info("Saving Graph - %s" % filename)
+        graph.saveGraphPajek(filename)
+
+
+
+
+
