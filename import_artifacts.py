31401f437c13e24c4fe0d8472cfa03b99d23d223
DarthNeo <james.m.morris@disney.com>
2014-08-16 00:01:04 -0400

    Updates
    Updates

--- import_artifacts.py
+++ import_artifacts.py
@@ -15,19 +15,26 @@ from nl_lib.Concepts import Concepts
 
 from lxml import etree
 
-namespaces={'xsi': 'http://www.w3.org/2001/XMLSchema-instance', 'archimate': 'http://www.archimatetool.com/archimate'}
+NS_MAP={'xsi': 'http://www.w3.org/2001/XMLSchema-instance', 'archimate': 'http://www.archimatetool.com/archimate'}
+XML_NS         =  NS_MAP["xsi"]
+ARCHIMATE_NS   =  NS_MAP["archimate"]
 
-XML_NS         =  "http://www.w3.org/2001/XMLSchema-instance"
-ARCHIMATE_NS   =  "http://www.archimatetool.com/archimate"
-NS_MAP = {"xsi": XML_NS, "archimate" : ARCHIMATE_NS}
+ARCHI_TYPE = "{%s}type" % NS_MAP["xsi"]
 
-ARCHI_TYPE = "{http://www.w3.org/2001/XMLSchema-instance}type"
+ArtifactColumns = ["Artifact ID", "Artifact Name", "Owner Artifact ID", "Owner Artifact Name"]
 
-Columns = ["Artifact ID", "Artifact Name", "Owner Artifact ID", "Owner Artifact Name"]
+CapabilityColumns = ["Capability Name","Capability Type","Capability Created Date","Capability Modified Date",
+                     "Capability Hex ID","Capability Comment","Parent Capability Hex ID"]
+
+FunctionColumns = ["Function Name","Function Created Date","Function Modified Date","Function Hex ID",
+                   "Function Comment","Function Org Unit Name","Function Org Unit Hex ID","Function Project Hex ID"]
+
+keyColumns = { "Artifact" : (0,2), "Capability" : (4, 6), "Function" : (4, 7) }
+nameColumns = { "Artifact" : (2,4), "Capability" : (0, 6), "Function" : (0, 6) }
+
 dictNode = dict()
 
-
-def insertArtifactRelations(p, row, se):
+def insertRelations(p, row, se, tag="element", type="archimate:AssociationRelationship"):
 
     #<folder name="Relations" id="c1ff528d" type="relations">
     # <element xsi:type="archimate:AssociationRelationship" id="b0341e0e" source="ea7c85c4" target="e38ecb1d"/>
@@ -35,55 +42,61 @@ def insertArtifactRelations(p, row, se):
     # Generate 8 digit Hex number
     id = str(hex(random.randint(0, 16777215)))[-6:] + str(hex(random.randint(0, 16777215))[-2:])
     name = row[1]
-    tag = "element"
-    type = "archimate:AssociationRelationship"
 
     if len(row) == 4 and len(row[2].strip()) > 0:
         attrib = dict()
         attrib["id"] = id
 
         try:
-            ArtifactID = row[0].decode(encoding='UTF-8',errors='ignore')
-            attrib["source"] = dictNode[ArtifactID]
+            colnum = keyColumns["Artifact"][0]
+            NodeID1 = row[colnum].decode(encoding='UTF-8',errors='ignore')
+            attrib["source"] = dictNode[NodeID1]
 
-            OwnerArtifactID = row[2].decode(encoding='UTF-8',errors='ignore')
-            attrib["target"] = dictNode[OwnerArtifactID]
+            colnum = keyColumns["Artifact"][1]
+            NodeID2 = row[colnum].decode(encoding='UTF-8',errors='ignore')
+            attrib["target"] = dictNode[NodeID2]
 
             attrib["{http://www.w3.org/2001/XMLSchema-instance}type"] = type
 
-            logger.info("%s::%s" % (row[0], row[1]))
+            logger.debug("%s::%s" % (row[0], row[1]))
 
             elm = etree.Element(tag, attrib, nsmap=NS_MAP)
             se[0].insert(0, elm)
 
+            return 1
+
         except:
-            logger.warn("Not Found : %s or %s" % (ArtifactID, OwnerArtifactID))
+            logger.warn("Not Found : %s or %s" % (NodeID1, NodeID2))
+
+    return 0
 
 
-def insertArtifacts(p, row, se):
+def insertNodes(p, row, se, tag="element", type="archimate:Artifact"):
     #<element xsi:type="archimate:Node" id="612a9b73" name="Linux Server"/>
 
     # Generate 8 digit Hex number
     id = str(hex(random.randint(0, 16777215)))[-6:] + str(hex(random.randint(0, 16777215))[-2:])
-    name = row[1]
-    tag = "element"
-    type = "archimate:Artifact"
 
     attrib = dict()
     attrib["id"] = id
-    attrib["name"] = row[1].decode(encoding='UTF-8',errors='ignore')
+    colnum = keyColumns["Artifact"][1]
+    attrib["name"] = row[colnum].decode(encoding='UTF-8',errors='ignore')
     attrib["{http://www.w3.org/2001/XMLSchema-instance}type"] = type
 
-    logger.info("%s::%s" % (row[0], row[1]))
+    logger.debug("%s::%s" % (row[0], row[1]))
 
     elm = etree.Element(tag, attrib, nsmap=NS_MAP)
     se[0].insert(0, elm)
 
-    #Columns = ["Artifact ID", "Artifact Name", "Owner Artifact ID", "Owner Artifact Name"]
-    dictNode[row[0].decode(encoding='UTF-8',errors='ignore')] = id
+    if type[:6] == "Artifact""
+        #Columns = ["Artifact ID", "Artifact Name", "Owner Artifact ID", "Owner Artifact Name"]
+        colnum = keyColumns["Artifact"][0]
+        dictNode[row[0].decode(encoding='UTF-8',errors='ignore')] = id
 
     ##<property key="a" value="b"/>
     #attrib["ArtifactID"]   =
+
+    return 1
 
 def insertIntoFolder(tree, folder, fileMetaEntity):
 
@@ -94,8 +107,7 @@ def insertIntoFolder(tree, folder, fileMetaEntity):
     se = tree.xpath(xp)
     p = se[0].getparent()
 
-    logger.info("%s - %d" % (se, len(se)))
-
+    count = 0
     rownum = 0
     for row in reader:
 
@@ -105,15 +117,17 @@ def insertIntoFolder(tree, folder, fileMetaEntity):
         else:
             rownum += 1
 
-        logger.info("%d" % rownum)
+        logger.debug("%d" % rownum)
 
         if len(row[0].strip()) == 0:
             break
 
         if folder == "Relations":
-            insertArtifactRelations(p, row, se)
+            count += insertRelations(p, row, se)
         else:
-            insertArtifacts(p, row, se)
+            count += insertNodes(p, row, se)
+
+    logger.info("%d inserted in %s" % (count, folder))
 
     file.close()
 
@@ -121,7 +135,9 @@ def outputXML(tree, filename="import_artifacts.archimate"):
     output = StringIO.StringIO()
     tree.write(output, pretty_print=True)
 
-    logger.info("%s" % (output.getvalue()))
+    logger.debug("%s" % (output.getvalue()))
+
+    logger.info("Saved to : %s" % filename)
 
     f = open(filename,'w')
     f.write(output.getvalue())
@@ -131,7 +147,8 @@ def outputXML(tree, filename="import_artifacts.archimate"):
 
 if __name__ == "__main__":
     fileArchimate = "/Users/morrj140/Documents/SolutionEngineering/DNX Phase 2/DNX Phase 2 0.8.archimate"
-    fileMetaEntity = "/Users/morrj140/Development/GitRepository/DirCrawler/Artifacts.csv"
+
+    fileMetaEntity = "/Volumes/user/Artifacts.csv"
 
     p, fname = os.path.split(fileArchimate)
     logger.info("Using : %s" % fileArchimate)
